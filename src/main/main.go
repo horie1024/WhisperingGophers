8e2ff99475bcfbe9a2de1db2554da3d41fd7565e
horie1024 <ryosuke.pic@gmail.com>
2014-09-23 16:30:27 +0900

    part1 complete

--- src/main/main.go
+++ src/main/main.go
@@ -9,14 +9,9 @@ package main
 
 import (
 	"bufio"
-	_ "compress/gzip"
-	_ "encoding/base64"
-	_ "encoding/json"
-	"fmt"
-	_ "io"
+	"encoding/json"
 	"log"
-	_ "os"
-	"strings"
+	"os"
 )
 
 type Message struct {
@@ -25,24 +20,29 @@ type Message struct {
 
 func main() {
 
-	s := bufio.NewScanner(strings.NewReader(input))
+	// Create a new bufio.Scanner reading from the standard input.
+	s := bufio.NewScanner(os.Stdin)
 
+	// Create a new json.Encoder writing into the standard output.
+	enc := json.NewEncoder(os.Stdout)
+
+	// Iterate over every line in the scanner
 	for s.Scan() {
-		fmt.Println(s.Text())
+
+		// Create a new message with the read text.
+		msg := Message{
+			Body: s.Text(),
+		}
+
+		// Encode the message, and check for errors!
+		err := enc.Encode(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+
+	// Check for a scan error.
 	if err := s.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	// TODO: Create a new bufio.Scanner reading from the standard input.
-	// TODO: Create a new json.Encoder writing into the standard output.
-	for /* TODO: Iterate over every line in the scanner */ {
-		// TODO: Create a new message with the read text.
-		// TODO: Encode the message, and check for errors!
-	}
-	// TODO: Check for a scan error.
 }
-
-const input = `A haiku is more
-Than just a collection of
-Well-formed syllables`
