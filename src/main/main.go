d09b805da0d3ed8053e2d1106201d969a4eef5f5
horie1024 <ryosuke.pic@gmail.com>
2014-09-23 16:51:19 +0900

    Flag

--- src/main/main.go
+++ src/main/main.go
@@ -1,17 +1,34 @@
-// Solution to part 1 of the Whispering Gophers code lab.
-// This program reads from standard input and writes JSON-encoded messages to
-// standard output. For example, this input line:
-//  Hello!
-// Produces this output:
-//  {"Body":"Hello!"}
+// Solution to part 2 of the Whispering Gophers code lab.
+//
+// This program extends part 1.
+//
+// It makes a connection the host and port specified by the -dial flag, reads
+// lines from standard input and writes JSON-encoded messages to the network
+// connection.
+//
+// You can test this program by installing and running the dump program:
+//  $ go get code.google.com/p/whispering-gophers/util/dump
+//  $ dump -listen=localhost:8000
+// And in another terminal session, run this program:
+//  $ part2 -dial=localhost:8000
+// Lines typed in the second terminal should appear as JSON objects in the
+// first terminal.
 //
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"log"
-	"os"
+	_ "bufio"
+	_ "encoding/json"
+	"flag"
+	"fmt"
+	_ "log"
+	_ "os"
+	"time"
+)
+
+var (
+	message = flag.String("message", "Hello!", "what to say")
+	delay   = flag.Duration("delay", 2*time.Second, "how long to wait")
 )
 
 type Message struct {
@@ -20,29 +37,7 @@ type Message struct {
 
 func main() {
 
-	// Create a new bufio.Scanner reading from the standard input.
-	s := bufio.NewScanner(os.Stdin)
-
-	// Create a new json.Encoder writing into the standard output.
-	enc := json.NewEncoder(os.Stdout)
-
-	// Iterate over every line in the scanner
-	for s.Scan() {
-
-		// Create a new message with the read text.
-		msg := Message{
-			Body: s.Text(),
-		}
-
-		// Encode the message, and check for errors!
-		err := enc.Encode(msg)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Check for a scan error.
-	if err := s.Err(); err != nil {
-		log.Fatal(err)
-	}
+	flag.Parse()
+	fmt.Println(*message)
+	time.Sleep(*delay)
 }
